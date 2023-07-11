13fda1489e23ef15a86cf67761b7d5b9672b38e3
Naveenlogs7 <naveenkumar763802@gmail.com>
2023-07-11 16:58:47 +0530

    tools done

---
+++ src/com/javapractise/Scanner_s1.java
@@ -0,0 +1,35 @@
+package com.javapractise;
+
+import java.util.Scanner;
+
+public class Scanner_s1{
+	
+	public static void main(String[] args) {
+	System.out.println("Enter the name:");
+	Scanner s = new Scanner(System.in);
+	String nextLine = s.nextLine();
+	System.out.println("Naveen kumar");
+	
+	
+	System.out.println("Enter the first name:");
+	String next = s.next();
+	System.out.println("Naveen");
+	
+	
+	System.out.println("Enter the last name:");
+	String lname = s.next();
+	System.out.println("kumar");
+	
+	
+	System.out.println("Enter your id:");
+	int id = s.nextInt();
+	System.out.println(1234);
+	
+	
+	
+	
+		
+	}
+}
+	
+	
